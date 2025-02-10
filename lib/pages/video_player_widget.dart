8fb77167df10c7b455c08ddc0ec4159e300c536d
Lunara-Jered <nemesislunaken@gmail.com>
2025-02-10 12:45:08 +0100

    Create video_player_widget.dart

---
+++ lib/pages/video_player_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+
+class VideoPlayerWidget extends StatefulWidget {
+  final String videoPath;
+  VideoPlayerWidget({required this.videoPath});
+
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+  late ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoPath)
+      ..initialize().then((_) {
+        setState(() {});
+      });
+
+    _chewieController = ChewieController(
+      videoPlayerController: _controller,
+      autoPlay: true,
+      looping: false,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("🎥 Lecture Vidéo")),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? Chewie(controller: _chewieController)
+            : CircularProgressIndicator(),
+      ),
+    );
+  }
+}
